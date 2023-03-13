fe6df8e023a375937aa9bd6ce42f3561816204e7
2021ct93045 <112265160+2021ct93045@users.noreply.github.com>
2023-03-13 20:15:43 +0530

    corrected code for sonar test

--- src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
+++ src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
@@ -8,8 +8,7 @@ import java.util.List;
 @RestController
 public class CustomerController {
     @GetMapping("/getAllCustomers")
-    public List<Customer> getAllCustomers(){
-        System.out.println("bad code checking for SonarQube code quality");
+    public List<Customer> getAllCustomers(){       
         Customer customer1=new Customer(100,"sachin");
         Customer customer2=new Customer(200,"ashish");
         return Arrays.asList(customer1,customer2);
