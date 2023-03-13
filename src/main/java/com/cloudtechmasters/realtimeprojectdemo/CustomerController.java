d64cd62073c9e635010e251ba2c6151d424dd26d
2021ct93045 <112265160+2021ct93045@users.noreply.github.com>
2023-03-13 20:32:08 +0530

    Update CustomerController.java

--- src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
+++ src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 @RestController
+@Slf4
 public class CustomerController {
     @GetMapping("/getAllCustomers")
     public List<Customer> getAllCustomers(){    
-        System.out.println("bad code check SonarQube");
+        log.info("good  code check SonarQube");
         Customer customer1=new Customer(100,"sachin");
         Customer customer2=new Customer(200,"ashish");
         return Arrays.asList(customer1,customer2);
